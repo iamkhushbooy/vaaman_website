62f204bfa461f67a66b864107cb84aded1bc5e5b
iamkhushbooy <khushbooygkp@gmail.com>
2026-04-03 11:44:15 +0530

    removed project section

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
             <AboutFacts/>
             <WhyChooseUs />
             <ServicesSection />
-            <ProjectsSection />
+            {/* <ProjectsSection /> */}
            <TeamSection/>
             <TestimonialsSection />
             <ClientsSection />
